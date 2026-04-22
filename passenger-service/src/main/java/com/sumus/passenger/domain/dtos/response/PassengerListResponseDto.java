6fbe158828263b9b298b45191b0f99ee0635c5e4
Joljolionn <joljolionn@gmail.com>
2026-04-21 21:26:04 -0300

    fix(passenger-service): PassengerListResponse continha lista de documentos ao inves de dtos

--- passenger-service/src/main/java/com/sumus/passenger/domain/dtos/response/PassengerListResponseDto.java
+++ passenger-service/src/main/java/com/sumus/passenger/domain/dtos/response/PassengerListResponseDto.java
@@ -2,11 +2,10 @@ package com.sumus.passenger.domain.dtos.response;
 
 import java.util.List;
 
-import com.sumus.passenger.domain.entities.PassengerDocument;
 
 public record PassengerListResponseDto(
 
-    List<PassengerDocument> passengers
+    List<PassengerResponseDto> passengers
 
 ) {
 }
