bcc65032a2817cc28bd451fdaf06f4adf6a1224f
Joljolionn <joljolionn@gmail.com>
2026-04-30 10:27:50 -0300

    feat(auth): configurar filtros de rota (permitAll)

---
+++ auth-service/src/main/java/com/sumus/auth_service/infra/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.sumus.auth_service.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(form -> form.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/**")
+            .permitAll())
+        .build();
+  }
+
+}
