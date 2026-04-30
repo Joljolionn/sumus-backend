dd74c67205cceea2b381bd879a89f5a913725507
Joljolionn <joljolionn@gmail.com>
2026-04-30 10:28:49 -0300

    feat(auth): configurar e expor chave RSA

---
+++ auth-service/src/main/java/com/sumus/auth_service/infra/config/RsaKeyConfig.java
@@ -0,0 +1,25 @@
+package com.sumus.auth_service.infra.config;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.nimbusds.jose.jwk.RSAKey;
+
+@Configuration
+public class RsaKeyConfig {
+
+    @Bean
+    public RSAKey rsaKey() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey((RSAPrivateKey) keyPair.getPrivate())
+                .keyID("sumus-key-id")
+                .build();
+    }
+}
