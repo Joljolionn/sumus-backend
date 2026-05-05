d810c62f505d04c173a821a052e0cfb56094cf91
Joljolionn <joljolionn@gmail.com>
2026-05-05 18:40:47 -0300

    feat(admin): criar base do servico de admin

---
+++ admin-service/src/test/java/com/sumus/admin_service/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumus.admin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
